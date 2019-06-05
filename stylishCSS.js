8f5f5bae7ce2b46963f79744cf29d435d06393f2
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-06-04 21:34:07 -0700

    finished stylishCSS.js and configured core.autocrlf to true

--- stylishCSS.js
+++ stylishCSS.js
@@ -4,8 +4,9 @@ const app = express();
 const port = process.argv[2];
 const htmlFile = process.argv[3];
 
+app.use(require('stylus').middleware(htmlFile));
 app.use(express.static(htmlFile));
-app.use(require('stylus').middleware('public'));
+
 
 app.listen(port);
 
@@ -14,6 +15,17 @@ Resources:
 https://github.com/stylus/stylus
 https://www.npmjs.com/package/express-stylus
 http://stylus-lang.com/docs/middleware.html
+
+------------Alternate Solution-------------
+
+const express = require('express')
+const app = express()
+
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
+
+
+app.listen(process.argv[2])
 
 
 */
